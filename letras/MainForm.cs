e44f2b063378a8c464aca42913872886cf4998ef
Tom Gregor <tgregor@ivsoftware.com>
2022-07-29 14:24:36 -0600

    Prelim OK.

--- letras/MainForm.cs
+++ letras/MainForm.cs
@@ -17,8 +17,10 @@ namespace letras
         protected override void OnLoad(EventArgs e)
         {
             base.OnLoad(e);
-                int alto = 100;
-                int ancho = 65;
+
+            // This code stays the same
+            int alto = 100;
+            int ancho = 65;
 
             for (byte fila = 0; fila < 5; fila++)
             {
@@ -27,22 +29,24 @@ namespace letras
                 letras.Width = ancho;
                 letras.BackColor = Color.Blue;
                 letras.ForeColor = Color.AntiqueWhite;
-                letras.Font = new Font("Arial", 60);
+                letras.Font = new Font("Arial", 28);
                 letras.TextAlign = ContentAlignment.MiddleCenter;
                 letras.BorderStyle = BorderStyle.FixedSingle;
                 letras.Left = 200 + (ancho + 20) * fila;
                 letras.Top = 60;
-                letras.Text = null;
+                letras.Text = _tstCount++.ToString();
                 letras.MouseMove += new MouseEventHandler(this.letras_MouseMove);
                 this.Controls.Add(letras);
             }
         }
+        char _tstCount = 'A';
+        // This code is different
         private void letras_MouseMove(object sender, MouseEventArgs e)
         {
             Label letra = (Label)sender;
             if(MouseButtons == MouseButtons.Left)
             {
-                Debug.WriteLine(e.Location);
+                // Here, your original calculation works OK once you have the correct Label object.
                 letra.Location = new Point(letra.Location.X + e.Location.X, letra.Location.Y + e.Location.Y);
             }
         }
